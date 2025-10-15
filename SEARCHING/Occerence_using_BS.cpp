cce03dc73491393b329b6105d140f91658c31c4e
Abu Nasim <abu_nasim1033@uits.edu.bd>
2025-10-16 01:04:30 +0600

    Add files via upload

---
+++ SEARCHING/Occerence_using_BS.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int BinarySearch(int ar[], int n, int target)
+{
+    int l = 0;
+    int r = n - 1;
+    int ans = -1;
+    while (l <= r)
+    {
+        int mid = (l + r) / 2;
+        if (ar[mid] == target)
+        {
+            ans = mid;
+            r = mid - 1; // First occurence ber korbe, pawar poreu check korbo left ki aro ache kina
+
+            l = mid + 1; //// Last occurence ber korbe, pawar poreu check korbo right ki aro ache kina
+
+            // Total Occurence = Last occurence - first occurence + 1 ;;
+        }
+        else if (ar[mid] < target)
+        {
+
+            l = mid + 1;
+        }
+        else
+        {
+
+            r = mid - 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int n, t;
+    cin >> n >> t;
+    int ar[n];
+    for (auto &u : ar)
+    {
+        cin >> u;
+    }
+    int ind = BinarySearch(ar, n, t);
+    if (ind == -1)
+    {
+        cout << "NOT FOUND" << endl;
+    }
+    else
+    {
+        cout << "FOUND at index " << ind << endl;
+    }
+    return 0;
+}
